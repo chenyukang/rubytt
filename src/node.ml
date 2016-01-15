1a623b9a061b81d0743c4af9b5dfad8dc4afd522
Yukang <moorekang@gmail.com>
2016-01-16 01:11:37 +0800

    more on node

--- src/node.ml
+++ src/node.ml
@@ -1,16 +1,48 @@
 
 type node_info = {
-  path: string;
-  file: string;
+  mutable path: string;
+  mutable file: string;
   ss: int;
   ee: int;
-  parent: node;
 }
 and
-  node =
+  node_type =
   | Nil
-  | IfNode of node_info * node * node * node
-  | IndexNode of node_info * node
-  | KwdNode of node_info * string * node
+  | IfNode of node * node * node
+  | IndexNode of node
+  | KwdNode of string * node
+  | Int of int
+  | String of string
+  | Symbol of string
+  | Void
+  | Name of string * string (* Fixme *)
+and
+  node = {
+  info: node_info;
+  ty: node_type;
+  mutable parent: node option;
+}
+
+let rec get_ast_root node =
+  match node.parent with
+  | Some(p) -> get_ast_root p
+  | _ -> node;;
+
+let set_node_parent node p =
+  node.parent <- Some(p)
+
+let set_node_file node f =
+  node.info.file <- f
+
+let set_node_path node p =
+  node.info.path <- p
+
+let make_nil_node file s e =
+  {
+    info = {path = ""; file = file; ss = s; ee = e};
+    ty = Nil;
+    parent = None;
+  }
+
 
 
