95078dffee95c7b3a9e2b1c6e65ef50f755da8cf
Yukang <moorekang@gmail.com>
2016-01-28 22:22:37 +0800

    readme

---
+++ src/demo.ml
@@ -0,0 +1,27 @@
+open Core.Std
+open Async.Std
+open Async_parallel_deprecated.Std
+
+let deep () =
+  Deferred.List.iter [ 1; 2; 10; 100; 1000 ] ~f:(fun depth ->
+    let rec loop i =
+      if i = 0
+      then return 0
+      else begin
+        Parallel.run (fun () -> loop (i - 1))
+        >>| function
+        | Error e -> failwith e
+        | Ok j -> j + 1
+      end
+    in
+    loop depth
+    >>| fun d ->
+    assert (d = depth))
+;;
+
+let () =
+  Parallel.init ();
+  (deep () >>> fun () -> Shutdown.shutdown 0);
+  never_returns (Scheduler.go ())
+;;
+
