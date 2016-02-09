b2e1c5651fc7a4050b87ca1efcf9d24f042a2d4c
Yukang <moorekang@gmail.com>
2016-02-10 00:14:03 +0800

    clean

--- src/main.ml
+++ src/main.ml
@@ -5,16 +5,9 @@ open Printer
 open Node
 open Util
 open Type
-open Typestack
 
 let () =
-  let a = TypeStack.empty in
-  let a = TypeStack.push a Nil (Int 2) in (
-    let b = TypeStack.pop a in
-    let b = TypeStack.push b (Int 2) Void in 
-    let e = TypeStack.size b in
-    Printf.printf "size: %d\n" e;
-    if Array.length Sys.argv <> 2 then
+  if Array.length Sys.argv <> 2 then
     Printf.eprintf "Usage: main filename\n"
   else
     let filename = Sys.argv.(1) in
@@ -22,4 +15,4 @@ let () =
     let ast = build_ast_from_file json in
     let ast_str = node_to_str ast 0 in
     Printf.printf "%s\n" ast_str
-  )
+  
