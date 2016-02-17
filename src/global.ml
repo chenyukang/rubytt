67d5c17289568866732f9ce832690de7de33018a
Yukang <moorekang@gmail.com>
2016-02-17 23:50:35 +0800

    add more for references

--- src/global.ml
+++ src/global.ml
@@ -1,4 +1,4 @@
-open Node
+open Core.Std;;
 
 type analyzer = {
   sid: string;
@@ -7,6 +7,7 @@ type analyzer = {
   global_table: Type.state_t;
   all_bindings: Type.binding_ty list;
   loaded_files: (string, bool) Hashtbl.t;
+  references: (Node.node, Type.binding_ty list) Hashtbl.t;
   path: string list;
 }
 
@@ -17,11 +18,25 @@ let make_analyzer () =
     project_dir = "";
     global_table = Type.global_table;
     all_bindings = [];
-    loaded_files = Hashtbl.create 1;
+    loaded_files = Hashtbl.Poly.create ();
+    references = Hashtbl.Poly.create ();
     path = [];
   }
 
 let global_Analyzer = make_analyzer();;
 
+let put_refs node bs =
+  let binded = Hashtbl.find global_Analyzer.references node in
+  match binded with
+  | None ->  (
+      Hashtbl.add_exn global_Analyzer.references ~key:node ~data: bs
+    )
+  | Some(v) -> (
+      List.iter bs ~f:(fun b -> Type.binding_add_ref b node);
+      ignore(Hashtbl.replace global_Analyzer.references ~key:node ~data:(v @ bs));
+    )
+
 let put_ref node bind =
-  ()
+  let bs = [bind] in
+  put_refs node bs
+
