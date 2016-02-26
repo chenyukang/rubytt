da29ab516fcba606e4fd6b5ddafe3bf7df1cfc9b
Yukang <moorekang@gmail.com>
2016-02-26 23:36:45 +0800

    add testcase

--- src/tests/quick_sort.rb
+++ src/tests/quick_sort.rb
@@ -1,6 +1,7 @@
-# def qsort lst
-#   return [] if lst.size() == 0
-#   less, more = left.partition {|x| x < first}
-#   return qsort(less) + [first] + qsort(more)
-# end
+def qsort lst
+  return [] if lst.size() == 0
+  # less, more = left.partition {|x| x < first}
+  less, more = left.partition ## bug now
+  return qsort(less) + [first] + qsort(more)
+end
 
