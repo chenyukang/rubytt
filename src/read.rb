7ab93c7929283c843768e9c691a7bdf5f7f9c021
Yukang Chen <moorekang@gmail.com>
2016-11-21 11:42:02 +0800

    add test cases

--- src/read.rb
+++
@@ -1,8 +0,0 @@
-f = File.read("./src/test.rb")
-
-f = f.encode('utf-8', { :undef => :replace,
-                      :invalid => :replace,
-                      :universal_newline => true })
-f.split.each{ |c|
-  puts "now: #{c.length}"
-}
