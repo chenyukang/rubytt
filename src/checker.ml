685b465ddb560655a3e4e190891b96c58a2e6fc7
Yukang <moorekang@gmail.com>
2016-11-26 08:19:11 +0800

    add checker

--- src/checker.ml
+++ src/checker.ml
@@ -17,22 +17,25 @@ let make_env () =
 let root_env = make_env();;
 
 let new_child parent =
+  Printf.printf "new child .........\n";
   let child = make_env() in
   parent.children <- parent.children @ [child];
   child
 
 let add_variable env var =
-  Hashtbl.add_exn env.variables ~key:var ~data:true
+  ignore(Hashtbl.add env.variables ~key:var ~data:true)
 
 let visited_variable env name =
-  Hashtbl.add_exn env.visited ~key:name ~data:true
+  ignore(Hashtbl.add env.visited ~key:name ~data:true)
 
 let rec env_info env =
+  Printf.printf "print env .........\n";
   Hashtbl.iter env.variables
                ~f:(fun ~key:v ~data:_ ->
                    let name = name_node_id v in
                    match Hashtbl.find env.visited name with
-                   | None -> (Printf.printf "unvisited variable: %s\n" name)
+                   | None ->
+                      (Printf.printf "unvisited variable: %s : %s\n" v.info.file name)
                    | _ -> ());
   List.iter env.children ~f:(fun e -> env_info e)
 
@@ -42,29 +45,43 @@ let check_unused asts =
     Printf.printf "%s\n" str;
     match ast.ty with
     | Block(stmts) -> (List.iter stmts ~f:(fun ast -> iter ast env))
-    | Class(n, _, body, _, _) -> (
+    | Class(_, _, body, _, _) | Module(_, _, body, _) ->  (
       let child = new_child env in
       iter body child
+    )
+    | Try(body, rescue, _else, final) -> (
+      List.iter [body; rescue; _else; final] ~f:(fun e -> iter e env)
     )
     | Func(info) -> (
       let child = new_child env in
       iter info.body child
     )
+    | Undef(nodes) -> List.iter nodes ~f:(fun e -> iter e env)
+    | BinOp(_, ln, rn) -> List.iter [ln; rn] ~f:(fun e -> iter e env)
+    | UnaryOp(_, operand) -> iter operand env
+    | Attribute(target, value) -> iter target env
+    | Subscript(value, slices) -> List.iter ([value] @ slices) ~f:(fun e -> iter e env)
+    | Handler(_, _, handler, _else) -> List.iter [handler; _else] ~f:(fun e -> iter e env)
+    | For(target, it, body) ->
+       List.iter [target; it; body] ~f:(fun e -> iter e env)
+    | If(test, body, _else) -> List.iter [test; body; _else] ~f:(fun e -> iter e env)
+    | While(test, body) -> List.iter [test; body] ~f:(fun e -> iter e env)
     | Name(id, _) -> (
       Printf.printf "lookup: %s\n" id;
       visited_variable env id
     )
     | Assign(target, value) -> (
-      match target.ty with
-      | Name(s, _) -> (
-        Printf.printf "set variable: %s\n" s;
-        add_variable env target
-      )
-      | _ -> ()
+      let _ = match target.ty with
+        | Name(s, _) -> (
+          Printf.printf "set variable: %s\n" s;
+          add_variable env target
+        )
+        | _ -> () in
+      iter value env
     )
-    | Call(func, pos, _, _) -> (
-      List.iter pos ~f:(fun x -> iter x env)
-    )
+    | Call(func, pos, _, block_arg) ->
+       List.iter (([func] @ pos) @ [block_arg]) ~f:(fun e -> iter e env)
+    | Kwd(_, v) | Return(v) | Starred(v) | Yield(v) -> iter v env
     | _ -> () in
   List.iter asts ~f:(fun ast -> iter ast root_env);
   env_info root_env
