0bd14aed48e472a3c83cc138ff2805849d45435f
Yukang <moorekang@gmail.com>
2016-11-26 10:42:20 +0800

    refactor check_unused

--- src/checker.ml
+++ src/checker.ml
@@ -7,8 +7,7 @@ type env = {
     mutable children: env list;
   }
 
-let make_env () =
-  {
+let make_env () = {
     visited = Hashtbl.Poly.create();
     variables = Hashtbl.Poly.create();
     children = [];
@@ -41,31 +40,24 @@ let rec env_info env =
 
 let check_unused asts =
   let rec iter ast env =
+    let _iter ast =
+      iter ast env in
     let str = Printer.node_to_str ast 0 in
     Printf.printf "%s\n" str;
     match ast.ty with
-    | Block(stmts) -> (List.iter stmts ~f:(fun ast -> iter ast env))
-    | Class(_, _, body, _, _) | Module(_, _, body, _) ->  (
-      let child = new_child env in
-      iter body child
-    )
-    | Try(body, rescue, _else, final) -> (
-      List.iter [body; rescue; _else; final] ~f:(fun e -> iter e env)
-    )
-    | Func(info) -> (
-      let child = new_child env in
-      iter info.body child
-    )
-    | Undef(nodes) -> List.iter nodes ~f:(fun e -> iter e env)
-    | BinOp(_, ln, rn) -> List.iter [ln; rn] ~f:(fun e -> iter e env)
-    | UnaryOp(_, operand) -> iter operand env
-    | Attribute(target, value) -> iter target env
-    | Subscript(value, slices) -> List.iter ([value] @ slices) ~f:(fun e -> iter e env)
-    | Handler(_, _, handler, _else) -> List.iter [handler; _else] ~f:(fun e -> iter e env)
-    | For(target, it, body) ->
-       List.iter [target; it; body] ~f:(fun e -> iter e env)
-    | If(test, body, _else) -> List.iter [test; body; _else] ~f:(fun e -> iter e env)
-    | While(test, body) -> List.iter [test; body] ~f:(fun e -> iter e env)
+    | Func(info) -> iter info.body (new_child env)
+    | Class(_, _, body, _, _) | Module(_, _, body, _) ->  iter body (new_child env)
+    | Block(stmts) -> List.iter stmts ~f:_iter
+    | Undef(nodes) -> List.iter nodes ~f:_iter
+    | BinOp(_, ln, rn) -> List.iter [ln; rn] ~f:_iter
+    | UnaryOp(_, operand) -> _iter operand
+    | Attribute(target, value) -> _iter target
+    | Handler(_, _, handler, _else) -> List.iter [handler; _else] ~f:_iter
+    | For(target, it, body) -> List.iter [target; it; body] ~f:_iter
+    | If(test, body, _else) -> List.iter [test; body; _else] ~f:_iter
+    | While(test, body) -> List.iter [test; body] ~f:_iter
+    | Subscript(value, slices) -> List.iter ([value] @ slices) ~f:_iter
+    | Try(body, rescue, _else, final) -> List.iter [body; rescue; _else; final] ~f:_iter
     | Name(id, _) -> (
       Printf.printf "lookup: %s\n" id;
       visited_variable env id
@@ -77,11 +69,10 @@ let check_unused asts =
           add_variable env target
         )
         | _ -> () in
-      iter value env
+      _iter value
     )
-    | Call(func, pos, _, block_arg) ->
-       List.iter (([func] @ pos) @ [block_arg]) ~f:(fun e -> iter e env)
-    | Kwd(_, v) | Return(v) | Starred(v) | Yield(v) -> iter v env
+    | Call(func, pos, _, block_arg) -> List.iter (([func] @ pos) @ [block_arg]) ~f:_iter
+    | Kwd(_, v) | Return(v) | Starred(v) | Yield(v) -> _iter v
     | _ -> () in
   List.iter asts ~f:(fun ast -> iter ast root_env);
   env_info root_env
