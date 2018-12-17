63311b63f1fb7a5f2fc4c28a956c498bec12a5e6
Yukang Chen <yukang.chen@dji.com>
2018-12-17 19:16:45 +0800

    fix ci

--- .ci/install.sh
+++ .ci/install.sh
@@ -4,10 +4,14 @@ set -e
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     brew install opam
+    brew install ruby
+    gem install ruby-progressbar
 fi
 
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     echo "y" | sudo apt-get install --force-yes ocaml ocaml-native-compilers camlp4-extra opam rubygems
+    sudo apt-get install -y ruby-dev
+    sudo gem install ruby-progressbar
 fi
 
 opam init -y
